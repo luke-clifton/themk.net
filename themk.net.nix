623f2e60193d0351234df1a3a6a4161badffd668
Luke Clifton <ltclifton@gmail.com>
2015-11-04 14:37:30 +1100

    Version bump

--- themk.net.nix
+++ themk.net.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, hakyll, stdenv }:
 mkDerivation {
   pname = "themk-net";
-  version = "0.1.0.0";
+  version = "0.1.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
